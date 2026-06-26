8d6188d98389274c6f182a27332a19b107620bcc
t91a60 <nastempny.0@gmail.com>
2026-06-26 14:36:59 +0200

    fix

--- src/hooks/useNotify.js
+++ src/hooks/useNotify.js
@@ -1,6 +1,10 @@
 import { useContext } from 'react'
 import { NotificationContext } from '../lib/notificationContext.js'
 
+const noop = () => {}
+const fallbackCtx = { notify: noop, dismiss: noop, clearAll: noop, toasts: [], panelOpen: false, setPanelOpen: noop }
+
 export function useNotify() {
-  return useContext(NotificationContext)
+  const ctx = useContext(NotificationContext)
+  return ctx || fallbackCtx
 }
